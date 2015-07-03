b6ef0771a7a3ffd97c822bbcf6a6bc776e57ca7e
oksigeno <jumaro@gmail.com>
2015-07-03 10:37:07 -0500

    initial commit

---
+++ todo_app/__openerp__.py
@@ -0,0 +1,13 @@
+{
+	'name': 'To-Do Application',
+	'description': 'Manage your personal Tasks with this module.',
+	'author': 'Modus S.A.',
+	'version': 'v0.1.0',
+	'depends': ['mail'],
+	'application': True,
+	'data': [
+		'todo_view.xml',
+		'security/ir.model.access.csv',
+		'security/todo_access_rules.xml',
+	],
+}
