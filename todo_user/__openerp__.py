53e4fbcdd92266d0e412b77c1524d519eca5bcec
oksigeno <jumaro@gmail.com>
2015-07-03 11:48:45 -0500

    03/07/2015 11:44

---
+++ todo_user/__openerp__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Multiuser To-Do',
+    'description': 'Extend the To-Do app to multiuser.',
+    'category': 'Human Resources',
+    'summary': 'Jobs, Departments, Employees Details',
+    'author': 'Modus S.A.',
+	'version': '0.1.0',
+    'depends': ['todo_app'],
+}
