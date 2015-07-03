2559e22f63373dcff739ce57917b741874fdebdd
oksigeno <jumaro@gmail.com>
2015-07-03 12:21:44 -0500

    03/07/2015 12:21

--- todo_user/todo_task.py
+++ todo_user/todo_task.py
@@ -5,3 +5,4 @@ class TodoTask(models.Model):
     _inherit = 'todo.task'
     user_id = fields.Many2one('res.users', 'Responsible')
     date_deadline = fields.Date('Deadline')
+    name = fields.Char(help="What needs to be done?")
