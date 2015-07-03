bfe72659cbfb2ad42e62ad3deaf0ccea4ca3b60d
oksigeno <jumaro@gmail.com>
2015-07-03 13:52:24 -0500

    03/07/2015 13:42

--- sri_app/__openerp__.py
+++ sri_app/__openerp__.py
@@ -3,6 +3,7 @@
 	'description': 'Modulo de integracion de Odoo 8.0 para exportar a MS Excel los datos del ATS en formato noviembre de 2014',
     'category': 'Accounting & Finance',
 	'author': 'Modus S.A.',
+    'summary': 'Modulo de integracion de Odoo 8.0 para exportar a MS Excel los datos del ATS en formato noviembre de 2014',
 	'version': '0.1.0',
     'depends': ['base', 'account_accountant'],
 	'application': False,
