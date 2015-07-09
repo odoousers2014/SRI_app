5e0adeb2d45934ac2f88b26f4ea8f611dda8ba64
oksigeno <jumaro@gmail.com>
2015-07-09 18:52:29 -0500

    09/07/2015 18:52

--- sql_model/sql_model.py
+++ sql_model/sql_model.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from openerp import tools
+
+class sql_model(models.Model):
+    _name = 'sql.report'
+    _auto = False
+    nombre = fields.Char('Cliente', readonly=True),
+    total = fields.Integer('Total', readonly=True)
+
+    def init(self, cr):
+        tools.sql.drop_view_if_exists(cr, 'sql_model')
+        cr.execute("""
+                    CREATE OR REPLACE VIEW "sql_model" AS (
+                    SELECT
+                    "public".res_partner."name" AS "nombre",
+                    Sum("public".account_invoice.amount_total) AS "total"
+                    FROM (account_invoice
+                    JOIN res_partner ON (((account_invoice.commercial_partner_id = res_partner.id) AND (account_invoice.partner_id = res_partner.id))))
+                    WHERE ((account_invoice.type)::text = 'out_invoice'::text)
+                    GROUP BY
+                    "public".res_partner."name"
+                    )
+        """)
+
+sql_model()
