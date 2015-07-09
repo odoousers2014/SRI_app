4f30eae00db17042b263d2ba0cee942311d1dddc
oksigeno <jumaro@gmail.com>
2015-07-09 13:54:57 -0500

    09/07/2015 13:54

---
+++ sql_model/__openerp__.py
@@ -0,0 +1,10 @@
+{
+	'name': 'SQL Query',
+	'description': 'Modulo de demostracion de consultas SQL',
+    'category': 'Accounting & Finance',
+	'author': 'Modus S.A.',
+    'summary': 'Modulo de demostracion de consultas SQL',
+	'version': '0.1.0',
+    'depends': ['base', 'account_accountant'],
+	'application': False,
+}
