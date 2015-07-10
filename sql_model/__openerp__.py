3c938ed37ac25f1ff98e12bbf46e3f15082a9cd4
oksigeno <jumaro@gmail.com>
2015-07-09 19:26:52 -0500

    09/07/2015 19:26

--- sql_model/__openerp__.py
+++ sql_model/__openerp__.py
@@ -6,5 +6,8 @@
     'summary': 'Modulo de demostracion de consultas SQL',
 	'version': '0.1.0',
     'depends': ['base', 'account_accountant'],
+	'data': [
+		'sql_view.xml',
+	],
 	'application': False,
 }
