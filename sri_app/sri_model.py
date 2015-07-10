3410358220c368508e67843aec8a1e3d1e00c7d3
oksigeno <jumaro@gmail.com>
2015-07-10 08:54:07 -0500

    10/07/2015 08:54

--- sri_app/sri_model.py
+++ sri_app/sri_model.py
@@ -4,4 +4,4 @@ from openerp import models, fields, api
 
 class res_partner(models.Model):
     _inherit = 'res.partner'
-    ruc = fields.Integer('RUC', help="Ingrese el número RUC", required=True)
+    numeroRuc = fields.Integer('RUC', help="Ingrese el número RUC", required=True)
