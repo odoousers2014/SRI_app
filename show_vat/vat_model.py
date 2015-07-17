9d4151b7caf358641d0f6942cfbd0ef91694bfe9
oksigeno <jumaro@gmail.com>
2015-07-16 20:02:58 -0500

    16/07/2015 20:02

--- show_vat/vat_model.py
+++ show_vat/vat_model.py
@@ -4,4 +4,4 @@ from openerp import models, fields, api
 
 class ShowVat(models.Model):
      _inherit = 'account.invoice'
-     vat = fields.Char(related='res_partner.vat', store=True)
+     vat = fields.Char(related='partner_id.vat', store=True)
