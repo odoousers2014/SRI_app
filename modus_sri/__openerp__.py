3185b6277ccf8f56773ad18b7d4fe01ca25d6733
oksigeno <jumaro@gmail.com>
2015-07-18 06:32:47 -0500

    18/07/2015 06:32

--- modus_sri/__openerp__.py
+++ modus_sri/__openerp__.py
@@ -4,6 +4,6 @@
 	'author': 'Modus S.A. - Juan Manuel Rosero Puente (09 9411 7082)',
 	'version': '0.1.0',
 	'depends': ['base', 'account_accountant'],
-	'application': True,
+	'application': False,
 	'data': [],
 }
