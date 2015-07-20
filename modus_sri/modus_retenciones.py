419cbdca18cbdfa7a37eb1c0ae0721d720d00c35
oksigeno <jumaro@gmail.com>
2015-07-20 12:51:17 -0500

    20/07/2015 12:51

--- modus_sri/modus_retenciones.py
+++ modus_sri/modus_retenciones.py
@@ -4,9 +4,20 @@ from openerp import models, fields, api
 class ModusRetenciones(models.Model):
     _name = 'modus.retenciones'
     name = fields.Char('Número de Factura', required=True)
+
 #    partner_id = fields.Many2one('res.partner', string='Cliente', required=True)
 #    date_invoice = fields.Date(string='Fecha de Emisión', required=True)
     date_invoice = fields.Date(string='Fecha de Emisión', required=True)
     partner_name = fields.Char('Nombre', required=True)
     partner_ruc = fields.Char('RUC', required=True)
     partner_address = fields.Char('Dirección', required=True)
+    total_retencion = fields.Char('Total Retención', required=True)
+
+class ModusRetencionesLine(models.Model):
+    _name = 'modus.retenciones.line'
+    ejercicio_fiscal = fields.Char('Ejercicio Fiscal', required=True)
+    base_imponible = fields.Char('Base Imponible', required=True)
+    codigo_impuesto = fields.Char('Código Impuesto', required=True)
+    impuesto_retencion = fields.Char('Impuesto', required=True)
+    porcentaje_retencion = ('Porcentaje de Retención', required=True)
+    valor_retencion = fields.Char('Valor Retención', required=True)
