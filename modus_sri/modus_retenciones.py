db1f7711556db575e2c9320881034c29f0adbb5f
oksigeno <jumaro@gmail.com>
2015-07-21 11:35:36 -0500

    21/07/2015 11:35

--- modus_sri/modus_retenciones.py
+++ modus_sri/modus_retenciones.py
@@ -11,12 +11,16 @@ class ModusRetenciones(models.Model):
     partner_ruc = fields.Char('RUC', required=True)
     partner_address = fields.Char('Dirección', required=True)
     total_retencion = fields.Char('Total Retención', required=True)
+    retencion_line_ids = fields.one2many('modus.retenciones.line', 'retencion_id', 'Inventory'),
 
 class ModusRetencionesLine(models.Model):
     _name = 'modus.retenciones.line'
+    retencion_id = fields.many2one('modus.retenciones', 'Inventory'),
     ejercicio_fiscal = fields.Char('Ejercicio Fiscal', required=True)
     base_imponible = fields.Char('Base Imponible', required=True)
     codigo_impuesto = fields.Char('Código Impuesto', required=True)
     impuesto_retencion = fields.Char('Impuesto', required=True)
     porcent_retencion = fields.Char('Porcentaje Retención', required=True)
     valor_retencion = fields.Char('Valor Retención', required=True)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
