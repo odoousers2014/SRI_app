5ba18e55883efebcb3052bd95df1a6786ca54658
oksigeno <jumaro@gmail.com>
2015-07-24 12:55:19 -0500

    24/07/2015 12:55

--- modus_sri/modus_retenciones.py
+++ modus_sri/modus_retenciones.py
@@ -4,23 +4,24 @@ from openerp import models, fields, api
 class ModusRetenciones(models.Model):
     _name = 'modus.retenciones'
     name = fields.Char('Número de Retención', required=True)
-    items = fields.One2many('modus.retenciones.line', 'name', 'Items')
+    items = fields.One2many('retenciones.line', 'name', 'Items')
 #    partner_id = fields.Many2one('res.partner', string='Cliente', required=True)
 #    date_invoice = fields.Date(string='Fecha de Emisión', required=True)
     date_invoice = fields.Date(string='Fecha de Emisión', required=True)
     partner_name = fields.Char('Nombre', required=True)
     partner_ruc = fields.Char('RUC', required=True)
     partner_address = fields.Char('Dirección', required=True)
-    total_retencion = fields.Float('Total Retención', compute='_compute_total_retencion', store=True)
+#    total_retencion = fields.Float('Total Retención', compute='_compute_total_retencion', store=True)
 
-    @api.one
-    @api.depends('valor_retencion')
-    def _compute_total_retencion(self):
-        for record in self:
-            record.total_retencion = sum(line.valor_retencion for line in record.modus_retenciones_line)
+#    @api.one
+#    @api.depends('retenciones_line.valor_retencion')
+#    def _compute_total_retencion(self):
+#        self.total_retencion = sum(line.valor_retencion for line in self.retenciones_line)
+#        for record in self:
+#            record.total_retencion = sum(line.valor_retencion for line in record.retenciones_line)
 
-class ModusRetencionesLine(models.Model):
-    _name = 'modus.retenciones.line'
+class RetencionesLine(models.Model):
+    _name = 'retenciones.line'
     name = fields.Char('Item')
 #    item_id = fields.Many2one('modus.retenciones', 'Número de Retención')
     ejercicio_fiscal = fields.Char('Ejercicio Fiscal')
