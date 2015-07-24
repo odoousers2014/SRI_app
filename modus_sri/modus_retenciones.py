992a280f03379a84d8ddffbc7f52de99e5b43ee2
oksigeno <jumaro@gmail.com>
2015-07-24 14:04:20 -0500

    24/07/2015 14:04

--- modus_sri/modus_retenciones.py
+++ modus_sri/modus_retenciones.py
@@ -1,34 +1,15 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
 
-class ModusRetenciones(models.Model):
-    _name = 'modus.retenciones'
-    name = fields.Char('Número de Retención', required=True)
-    items = fields.One2many('retenciones.line', 'name', 'Items')
-#    partner_id = fields.Many2one('res.partner', string='Cliente', required=True)
-#    date_invoice = fields.Date(string='Fecha de Emisión', required=True)
-    date_invoice = fields.Date(string='Fecha de Emisión', required=True)
-    partner_name = fields.Char('Nombre', required=True)
-    partner_ruc = fields.Char('RUC', required=True)
-    partner_address = fields.Char('Dirección', required=True)
-#    total_retencion = fields.Float('Total Retención', compute='_compute_total_retencion', store=True)
-
-#    @api.one
-#    @api.depends('retenciones_line.valor_retencion')
-#    def _compute_total_retencion(self):
-#        self.total_retencion = sum(line.valor_retencion for line in self.retenciones_line)
-#        for record in self:
-#            record.total_retencion = sum(line.valor_retencion for line in record.retenciones_line)
-
-class RetencionesLine(models.Model):
-    _name = 'retenciones.line'
+class ModusRetencionesLine(models.Model):
+    _name = 'modus.retenciones.line'
     name = fields.Char('Item')
 #    item_id = fields.Many2one('modus.retenciones', 'Número de Retención')
     ejercicio_fiscal = fields.Char('Ejercicio Fiscal')
     base_imponible = fields.Float('Base Imponible', required=True)
     codigo_impuesto = fields.Char('Código Impuesto')
     impuesto_retencion = fields.Char('Impuesto')
-    porcent_retencion = fields.Integer('Porcentaje Retención', required=True)
+    porcent_retencion = fields.Float('Porcentaje Retención', required=True)
     valor_retencion = fields.Float('Valor Retención', compute='_compute_valor_retencion', store=True)
 
     @api.one
@@ -36,5 +17,24 @@ class RetencionesLine(models.Model):
     def _compute_valor_retencion(self):
         for record in self:
             record.valor_retencion = record.base_imponible * (record.porcent_retencion / 100)
+
+class ModusRetenciones(models.Model):
+    _name = 'modus.retenciones'
+    name = fields.Char('Número de Retención', required=True)
+    items = fields.One2many('modus.retenciones.line', 'name', 'Items')
+#    partner_id = fields.Many2one('res.partner', string='Cliente', required=True)
+#    date_invoice = fields.Date(string='Fecha de Emisión', required=True)
+    date_invoice = fields.Date(string='Fecha de Emisión', required=True)
+    partner_name = fields.Char('Nombre', required=True)
+    partner_ruc = fields.Char('RUC', required=True)
+    partner_address = fields.Char('Dirección', required=True)
+    total_retencion = fields.Float('Total Retención', compute='_compute_total_retencion', store=True)
+
+    @api.one
+    @api.depends('modus_retenciones_line.valor_retencion')
+    def _compute_total_retencion(self):
+#        self.total_retencion = sum(line.valor_retencion for line in self.modus_retenciones_line)
+        for record in self:
+            record.total_retencion = sum(line.valor_retencion for line in record.modus_retenciones_line)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
