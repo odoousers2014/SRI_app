3139611cb42f9cdeb26da6555df4ef8375fb62c0
oksigeno <jumaro@gmail.com>
2015-07-24 09:35:27 -0500

    24/07/2015 09:35

--- modus_sri/__openerp__.py
+++ modus_sri/__openerp__.py
@@ -1,7 +1,7 @@
 {
 	'name': 'Modus SRI',
 	'description': 'Localizacion de Odoo para SRI-ATS en Ecuador.',
-	'author': 'Modus S.A. - Juan Manuel Rosero Puente (09 9411 7082)',
+	'author': 'Modus S.A. - Juan Manuel Rosero Puente +593 (9) 9411 7082',
 	'version': '0.1.0',
 	'depends': ['base', 'account_accountant'],
 	'application': False,
